9c00e35f0f3e55319f9ca7432b8e864224ce0201
SuperVegetoo <vegetoo255@gmail.com>
2020-09-07 13:54:59 +0100

    Add callable Filter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="directory-listing-download",
+    name="directory_downloader",
     version="0.0.1",
     author="Super Vegetoo",
     author_email="vegetoo255@gmail.com",
